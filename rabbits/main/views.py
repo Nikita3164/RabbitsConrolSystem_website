234d064aeb74dde1ee973a632e1529cafc33d3a1
Nikita Bulankin <fhgrty30@gmail.com>
2023-06-12 15:24:01 +0300

    Установка проекта на django

---
+++ rabbits/main/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'main/base.html')
+
+
+def setup(request):
+    return render(request, 'main/setup.html')
